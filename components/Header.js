4025e3509149aa6b7bc5273e17dd0f6096d6168a
Vladimir Pejic <vladimir.pejic@gmail.com>
2021-12-05 00:24:16 +0100

    Header complete

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,11 @@
 import Image from 'next/image';
-import {
-    SearchIcon,
-    PlusCircleIcon,
-    UserGroupIcon,
-    HeartIcon,
-    PaperAirplaneIcon,
-    MenuIcon
-} from "@heroicons/react/outline";
+import {SearchIcon, PlusCircleIcon, UserGroupIcon, HeartIcon, PaperAirplaneIcon, MenuIcon} from "@heroicons/react/outline";
+import {HomeIcon} from "@heroicons/react/solid";
 
 function Header() {
     return (
-        <div>
-            <div className='flex justify-between max-w-6xl'>
+        <div className="shadow-sm border-b bg-white sticky top-0 z-50">
+            <div className='flex justify-between max-w-6xl mx-5 lg:mx-auto'>
 
                 <div className='relative hidden lg:inline-grid w-24 cursor-pointer'>
                     <Image src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
@@ -19,7 +13,6 @@ function Header() {
                            objectFit="contain"
                     />
                 </div>
-
                 <div className='relative w-10 lg:hidden flex-shrink-0 cursor-pointer'>
                     <Image src="https://www.marjanvanaubel.com/wp-content/uploads/2019/11/instagram-logo.png"
                            layout="fill"
@@ -28,11 +21,32 @@ function Header() {
                 </div>
 
                 {/* Search */}
-                <div className="relative mt-1 p-3 rounded-md">
-                    <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
-                        <SearchIcon className="h-5 w-5 text-gray-500" />
+                <div className="max-w-xs">
+                    <div className="relative mt-1 p-3 rounded-md">
+                        <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+                            <SearchIcon className="h-5 w-5 text-gray-500"/>
+                        </div>
+                        <input type="text"
+                               className="bg-gray-100 w-full pl-10 sm:text-sm border-gray-300 rounded-md focus:ring-black focus:border-black"
+                               placeholder="Search"/>
                     </div>
-                    <input type="text" className="bg-gray-100 w-full pl-10 sm:text-sm border-gray-300 rounded-md focus:ring-black focus:border-black" placeholder="Search" />
+                </div>
+
+
+                {/* Right */}
+                <div className="flex items-center justify-end space-x-4">
+                    <HomeIcon className="navBtn" />
+                    <MenuIcon className="h-6 md:hidden cursor-pointer" />
+
+                    <div className="relative navBtn">
+                        <PaperAirplaneIcon className="navBtn rotate-45" />
+                        <div className="absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">3</div>
+                    </div>
+                    <PlusCircleIcon className="navBtn" />
+                    <UserGroupIcon className="navBtn" />
+                    <HeartIcon className="navBtn" />
+
+                    <img className="h-10 rounded-full cursor-pointer" src="https://media-exp1.licdn.com/dms/image/C4D03AQEwNDCHSZqOlQ/profile-displayphoto-shrink_200_200/0/1626449741844?e=1642636800&v=beta&t=Uw8v6b18JzlPwxX9aXMUOsVZ0IsoVY5ZE9Nwy-QX6kQ" ></img>
                 </div>
 
             </div>
